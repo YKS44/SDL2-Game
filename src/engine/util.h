61488d54cb16eca8973a951fa5be654977bd2581
YKS44 <79933420+YKS44@users.noreply.github.com>
2024-05-06 13:51:05 -0700

    add comments to util functions

--- src/engine/util.h
+++ src/engine/util.h
@@ -6,6 +6,8 @@
 #define ERROR_EXIT(...) { fprintf(stderr, __VA_ARGS__); exit(1); }
 #define ERROR_RETURN(R, ...) { fprintf(stderr, __VA_ARGS__); return R; }
 
-//
+//Returns a random number between [start,end)
 i32 random_range(i32 start, i32 end);
+
+//Delays the program for given amount of milliseconds.
 void delay(i32 millisec);
