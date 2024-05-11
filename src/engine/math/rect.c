c58f725fc3d98666602e056f0542ebf748639b4f
YKS44 <79933420+YKS44@users.noreply.github.com>
2024-05-11 13:50:31 -0700

    make function prototypes

--- src/engine/math/rect.c
+++ src/engine/math/rect.c
@@ -1,2 +1,13 @@
 #include "../rect.h"
 
+Rect rect_minkowski_diff(Rect rect1, Rect rect2){
+    Rect diff;
+
+    diff.pos.x = rect1.pos.x - rect2.pos.x;
+    diff.pos.y = rect2.pos.y - rect2.pos.y;
+
+    diff.h = (rect1.h+rect2.h)/2;
+    diff.w = (rect1.w+rect2.w)/2;
+    
+    return diff;
+}
--- src/engine/rect.h
+++ src/engine/rect.h
@@ -2,9 +2,14 @@
 
 #include "vector.h"
 #include "types.h"
+#include <stdbool.h>
+#include "slime.h"
 
 typedef struct {
     Vec2 pos;
     u32 w;
     u32 h;
 } Rect;
+
+Rect rect_minkowski_diff(Rect rect1, Rect rect2);
+bool rect_intersect_point(Rect rect, Point point);
